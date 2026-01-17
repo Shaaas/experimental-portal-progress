8c673509cfff36cb3165a09926be745bb45ba2c4
Sharon Mutanu <mutanusharon0@gmail.com>
2026-01-17 18:18:17 +0300

    Initialize Express server with time endpoint
    Set up basic Express server with a route to get current time from the database.

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json({ time: result.rows[0] });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
 // Server setup coming soon
 
